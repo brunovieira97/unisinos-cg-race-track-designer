1b3d5d917a30de421aae2a7b7ff92f1993fd0159
Christian Lima <christianmellolima@gmail.com>
2018-11-27 22:23:52 -0200

    created a workaround to test the b-spline creation

--- Shaders/Vertex.glsl
+++ Shaders/Vertex.glsl
@@ -1,10 +1,11 @@
-#version 410
+#version 410 core
 
 layout (location = 0) in vec3 v;
 
 uniform mat4 projection;
 
 void main()
-{
-    gl_Position = projection * vec4(v.x, v.y, v.z, 1.0);
+{    
+    gl_Position = projection * vec4(v.x, v.y, 0.0, 1.0);   
+
 };
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -12,12 +12,15 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 void MouseCallback(GLFWwindow* window, int button, int action, int mods);
+vector<glm::vec3*>* CreateBSpline(vector<glm::vec3*>* points);
 
 vector<float> finalPointsFloat;
+vector<glm::vec3*>* selectedPoints = new vector<glm::vec3*>();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+int click = 0;
 
 glm::mat4 projection = glm::ortho(0.0f, 800.0f, 600.0f, 0.0f, -1.0f, 1.0f);
 
@@ -27,19 +30,33 @@ void MouseCallback(GLFWwindow* window, int button, int action, int mods) {
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
 		double xpos, ypos;
 		glfwGetCursorPos(window, &xpos, &ypos);
+		
+		glm::vec3* point = new glm::vec3(xpos, ypos, 0.0);
+		selectedPoints->push_back(point);
 
-		finalPointsFloat.push_back(xpos);
-		finalPointsFloat.push_back(ypos);
-		finalPointsFloat.push_back(0.0);
+		// finalPointsFloat.push_back(xpos);
+		// finalPointsFloat.push_back(ypos);
+		// finalPointsFloat.push_back(0.0);
 		cout << "ponto registrado" << endl;
 		cout << "x = " << xpos << endl;
 		cout << "y = " << ypos << endl;
-	
+
+		if (click > 2){
+			CreateBSpline(selectedPoints);
+		}
+		
+
 		glBindVertexArray(vao);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*finalPointsFloat.size(), &finalPointsFloat[0], GL_STATIC_DRAW);		
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*finalPointsFloat.size(), finalPointsFloat.data(), GL_STATIC_DRAW);		
 		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		// int size = finalPointsFloat.size();
+		// for(int i = 0; i < size; i++){
+		// 	printf("%f\n",finalPointsFloat[i]);
+		// }
+
+		click++;
 	 }
 }
 
@@ -76,10 +93,11 @@ int main(){
 		return EXIT_FAILURE;
 	}
 
-	Shader shader("Shaders/Vertex.glsl", "Shaders/Fragment.glsl");
-	shader.use();
+	// Shader shader("Shaders/Vertex.glsl", "Shaders/Fragment.glsl");
+	// shader.use();
 
 	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 	glGenBuffers(1, &vbo);
 
     // render loop
@@ -98,15 +116,19 @@ int main(){
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
 
-		glBindVertexArray(vao);
+		Shader shader("Shaders/Vertex.glsl", "Shaders/Fragment.glsl");
+		shader.use();
 
 		glEnable(GL_POINT_SMOOTH);
 		glPointSize(15);
-		glDrawArrays(GL_POINTS, 0, finalPointsFloat.size());
 		shader.setMat4("projection", projection);
+		//glDrawArrays(GL_POINTS, 0, finalPointsFloat.size());
+		glDrawArrays(GL_LINE_STRIP, 0, finalPointsFloat.size());
 		glDisable(GL_POINT_SMOOTH);
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+		
     }
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
@@ -128,4 +150,51 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+
+vector<glm::vec3*>* CreateBSpline(vector<glm::vec3*>* points) {
+
+	vector<glm::vec3*>* bspline = new vector<glm::vec3*>();
+	vector<glm::vec3*>* temp = new vector<glm::vec3*>();
+	vector<glm::vec3*>* temp2 = new vector<glm::vec3*>();
+
+	int size = points->size();
+	for (int i = 0; i < size ; i++) {
+		temp->push_back(new glm::vec3(points->at(i)->x, points->at(i)->y, 0));
+	}
+
+
+	temp->push_back(points->at(0));
+	temp->push_back(points->at(1));
+	temp->push_back(points->at(2));
+
+
+	for (int i = 0; i < (temp->size() - 3); i++) {
+
+		for (int j = 0; j<100; ++j){
+
+			float t = static_cast<float>(j)/99.0;
+		
+			GLfloat x = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1)*temp->at(i)->x +
+				(3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4)*temp->at(i+1)->x +
+				(-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1)*temp->at(i+2)->x +
+				(1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0)*temp->at(i+3)->x) / 6);
+
+			GLfloat y = (((-1 * pow(t, 3) + 3 * pow(t, 2) - 3 * t + 1)*temp->at(i)->y +
+				(3 * pow(t, 3) - 6 * pow(t, 2) + 0 * t + 4)*temp->at(i+1)->y +
+				(-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1)*temp->at(i+2)->y +
+				(1 * pow(t, 3) + 0 * pow(t, 2) + 0 * t + 0)*temp->at(i+3)->y) / 6);
+					
+			glm::vec3* point = new glm::vec3(x, y, 0.0);
+			bspline->push_back(point);
+			finalPointsFloat.push_back(x);
+			finalPointsFloat.push_back(y);
+			finalPointsFloat.push_back(0);
+
+			bspline->push_back(new glm::vec3(1.0, 1.0, 1.0));
+		}	
+	}
+
+	return bspline;
 }
